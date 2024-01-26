beeea4082cc1cd1d97f2232fe7f514e0324eff16
Alexander <alexvl1972@hotmail.com>
2024-01-26 20:15:32 +1000

    Update device.mk

--- device.mk
+++ device.mk
@@ -89,6 +89,7 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libpuresoftkeymasterdevice
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
